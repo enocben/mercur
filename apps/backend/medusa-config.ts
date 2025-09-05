7788ac502b9f37d276259d6e4b97223900102052
enocben <benenoc5@gmail.com>
2025-09-05 19:15:29 +0200

    Addition of the Redis server URL

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -5,6 +5,7 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -82,12 +83,6 @@ module.exports = defineConfig({
             }
           }
         ]
-      }
-    },
-    {
-      resolve: '@medusajs/medusa/cache-redis',
-      options: {
-        redisUrl: process.env.REDIS_URL
       }
     },
   ]
