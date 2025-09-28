f7e9bf97d56acac0769e2a34d5e8271e702110de
enocben <benenoc5@gmail.com>
2025-09-28 11:09:43 +0200

    temp: disable Resend email service for development
    - Comment out Resend client initialization and email sending
    - Add TODO comment explaining sandbox mode limitation
    - Return empty object instead of sending actual emails
    - This allows development without domain verification requirements

--- packages/modules/resend/src/service.ts
+++ packages/modules/resend/src/service.ts
@@ -1,12 +1,8 @@
-import { Resend } from 'resend'
+// import { Resend } from 'resend'
 
-import {
-  AbstractNotificationProviderService,
-  MedusaError
-} from '@medusajs/framework/utils'
+import { AbstractNotificationProviderService, MedusaError } from '@medusajs/framework/utils'
 import { ProviderSendNotificationDTO } from '@medusajs/types'
 
-import { emailTemplates } from './email-templates'
 
 type ResendOptions = {
   api_key: string
@@ -15,14 +11,14 @@ type ResendOptions = {
 
 class ResendNotificationProviderService extends AbstractNotificationProviderService {
   static identifier = 'notification-resend'
-  private resendClient: Resend
-  private options: ResendOptions
+  // private resendClient: Resend
+  // private options: ResendOptions
 
   constructor(_, options: ResendOptions) {
     super()
     this.validateModuleOptions(options)
-    this.resendClient = new Resend(options.api_key)
-    this.options = options
+    // this.resendClient = new Resend(options.api_key)
+    // this.options = options
   }
 
   validateModuleOptions(options: ResendOptions) {
@@ -37,7 +33,15 @@ class ResendNotificationProviderService extends AbstractNotificationProviderServ
   }
 
   async send(notification: ProviderSendNotificationDTO) {
-    const { data, error } = await this.resendClient.emails.send({
+    // TODO(enoc): ⚠️ Resend is currently running in sandbox mode,
+    // emails can only be sent to my address (benenoc5@gmail.com).
+    // To enable production emails:
+    // 1. Add and verify a custom domain at resend.com/domains
+    // 2. Update the `from` address to use this verified domain
+    // 3. Run Medusa with NODE_ENV=production
+
+
+    /*const { data, error } = await this.resendClient.emails.send({
       from: notification.from?.trim() || this.options.from,
       to: notification.to,
       subject: notification.content?.subject as string,
@@ -53,9 +57,10 @@ class ResendNotificationProviderService extends AbstractNotificationProviderServ
         MedusaError.Types.UNEXPECTED_STATE,
         'No data returned by resend client'
       )
-    }
+    }*/
 
-    return data
+    // return data
+    return {}
   }
 }
 
